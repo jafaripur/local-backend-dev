45d423605127aae49f190593c08912f687959dc4
Araz <mjafaripur@yahoo.com>
2021-09-18 20:06:49 +0430

    Enable password in elasticsearch.

--- playbooks/application/templates/docker/elasticsearch/start.sh
+++ playbooks/application/templates/docker/elasticsearch/start.sh
@@ -7,6 +7,7 @@ docker run -it -d \
            -e "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
            -e "bootstrap.memory_lock=true" \
            -e "ELASTIC_PASSWORD={{vault_elasticsearch_password}}" \
+           -e "xpack.security.enabled=true" \
            -v {{docker_files_path}}/elasticsearch/data:/usr/share/elasticsearch/data \
            --restart unless-stopped \
            --log-driver json-file \
