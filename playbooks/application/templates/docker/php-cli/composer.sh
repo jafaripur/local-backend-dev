9a0357518c8ce75bda3114a958bce8ce78ff8d03
Araz <mjafaripur@yahoo.com>
2021-09-24 14:46:30 +0330

    isolate php composer cache folder to current folder not public in host /home/araz directory.

--- playbooks/application/templates/docker/php-cli/composer.sh
+++ playbooks/application/templates/docker/php-cli/composer.sh
@@ -2,20 +2,20 @@
   
 if [ "$1" = "update" ]; then
     docker run --rm --interactive --tty \
-	  -v $HOME/.composer:/tmp \
-	  -v $PWD:/app \
+	  -v "{{docker_files_path}}/php-cli/data/composer:/tmp" \
+	  -v "$PWD:/app" \
 	  -u $(id -u ${USER}):$(id -g ${USER}) \
 	  composer update --ignore-platform-reqs --no-scripts --optimize-autoloader --classmap-authoritative
 elif [ "$1" = "install" ]; then
 	docker run --rm --interactive --tty \
-	  -v $HOME/.composer:/tmp \
-	  -v $PWD:/app \
+	  -v "{{docker_files_path}}/php-cli/data/composer:/tmp" \
+	  -v "$PWD:/app" \
 	  -u $(id -u ${USER}):$(id -g ${USER}) \
 	  composer install --ignore-platform-reqs --no-scripts --optimize-autoloader --classmap-authoritative
 else
     docker run --rm --interactive --tty \
-	  -v $HOME/.composer:/tmp \
-	  -v $PWD:/app \
+	  -v "{{docker_files_path}}/php-cli/data/composer:/tmp" \
+	  -v "$PWD:/app" \
 	  -u $(id -u ${USER}):$(id -g ${USER}) \
 	  composer "$@"
 fi
