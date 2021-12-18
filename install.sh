fc4c24a09ba462ec1e1e3cb72f94eca9341c48d1
Araz <mjafaripur@yahoo.com>
2021-12-19 01:30:20 +0330

    Change pip installing from root user to normal user.

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 sudo apt-get install python3 python3-dist-utils python3-apt -y
 
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python3 get-pip.py
-sudo python3 -m pip install ansible
+python3 get-pip.py --user
 rm get-pip.py
+python3 -m pip install --user ansible
 
 ansible-galaxy role install -r ./requirements.yml --force
 ansible-galaxy collection install -r ./requirements.yml --force
