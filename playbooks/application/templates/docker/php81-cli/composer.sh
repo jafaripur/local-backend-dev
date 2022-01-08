e04fa93e26118aa3e1aa3bc83d76277db10b7899
Araz <mjafaripur@yahoo.com>
2022-01-09 02:29:37 +0330

    Install extra ext for php docker.

--- playbooks/application/templates/docker/php81-cli/composer.sh
+++ playbooks/application/templates/docker/php81-cli/composer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
   
 if [ "$1" = "update" ]; then
-    docker run --rm --interactive --tty \
+    docker run --rm --init --interactive --tty \
 	  -v {{docker_files_path}}/php81-cli/config/php-custom.ini:/usr/local/etc/php/conf.d/99-php-custom.ini:ro \
 	  -v "{{docker_files_path}}/php81-cli/composer:/.composer" \
 	  -v "$PWD:/var/www/html" \
@@ -9,7 +9,7 @@ if [ "$1" = "update" ]; then
 	  -u $(id -u ${USER}):$(id -g ${USER}) \
 	  {{docker_php81cli_image_build}}:latest composer update --optimize-autoloader --classmap-authoritative
 elif [ "$1" = "install" ]; then
-	docker run --rm --interactive --tty \
+	docker run --rm --init --interactive --tty \
 	  -v {{docker_files_path}}/php81-cli/config/php-custom.ini:/usr/local/etc/php/conf.d/99-php-custom.ini:ro \
 	  -v "{{docker_files_path}}/php81-cli/composer:/.composer" \
 	  -v "$PWD:/var/www/html" \
@@ -17,7 +17,7 @@ elif [ "$1" = "install" ]; then
 	  -u $(id -u ${USER}):$(id -g ${USER}) \
 	  {{docker_php81cli_image_build}}:latest composer install --optimize-autoloader --classmap-authoritative
 else
-    docker run --rm --interactive --tty \
+    docker run --rm  --init --interactive --tty \
 	  -v {{docker_files_path}}/php81-cli/config/php-custom.ini:/usr/local/etc/php/conf.d/99-php-custom.ini:ro \
 	  -v "{{docker_files_path}}/php81-cli/composer:/.composer" \
 	  -v "$PWD:/var/www/html" \
