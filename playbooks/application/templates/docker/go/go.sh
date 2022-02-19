eb142b4aa67cfa7497dfe4dfcb7595c759b489d2
Araz <mjafaripur@yahoo.com>
2022-02-19 20:25:55 +0330

    Add internal network in run go container.

--- playbooks/application/templates/docker/go/go.sh
+++ playbooks/application/templates/docker/go/go.sh
@@ -6,6 +6,7 @@ GOARCH=amd64 #386
 
 if [ "$1" = "make" ]; then
 	docker run --rm \
+           --network {{docker_network_internal}} \
            -v "${PWD}:/usr/src/myapp" \
            -v {{docker_files_path}}/go/cache:/root/.cache \
            -v {{docker_files_path}}/go/gopath:/go \
@@ -15,7 +16,9 @@ if [ "$1" = "make" ]; then
            --memory {{docker_go_memory}} \
            {{docker_go_image_build}} \
            make "$@"
+else
     docker run -it --init --rm \
+           --network {{docker_network_internal}} \
            -p 127.0.0.1:$PORT:8080 \
            -v "${PWD}:/usr/src/myapp" \
            -v {{docker_files_path}}/go/cache:/root/.cache \
@@ -24,6 +27,6 @@ if [ "$1" = "make" ]; then
            -e GOOS=$GOOS \
            -e GOARCH=$GOARCH \
            --memory {{docker_go_memory}} \
-           {{docker_go_image_build}} \
+           {{docker_go_image_build}}:latest \
            go "$@"
 fi
