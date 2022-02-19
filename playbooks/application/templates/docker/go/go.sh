fe52d6f2e940f7c57075c905b44ceda23e1a0a57
Araz <mjafaripur@yahoo.com>
2022-02-19 18:56:44 +0330

    Change PHP version, refactor go shell command.

--- playbooks/application/templates/docker/go/go.sh
+++ playbooks/application/templates/docker/go/go.sh
@@ -15,6 +15,18 @@ if [ "$1" = "make" ]; then
            --memory {{docker_go_memory}} \
            {{docker_go_image}} \
            make "$@"
+elif [ "$1" = "vscode" ]; then
+    docker run --init --rm \
+           -p 127.0.0.1:$PORT:8080 \
+           -v "${PWD}:/usr/src/myapp" \
+           -v {{docker_files_path}}/go/cache:/.cache \
+           -v {{docker_files_path}}/go/gopath:/go \
+           --workdir /usr/src/myapp \
+           -e GOOS=$GOOS \
+           -e GOARCH=$GOARCH \
+           --memory {{docker_go_memory}} \
+           {{docker_go_image}} \
+           go "$@"
 else
     docker run -it --init --rm \
            -p 127.0.0.1:$PORT:8080 \
